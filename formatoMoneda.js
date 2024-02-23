7a3a41806612ce6d60814a528d1790fb270ba896
angel chacin <111093093+chacin04@users.noreply.github.com>
2024-02-23 15:18:29 -0400

    Create formatoMoneda.js

---
+++ formatoMoneda.js
@@ -0,0 +1,6 @@
+const formatearMoneda = (monto) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD', // Puedes ajustar la moneda según tus necesidades
+    }).format(monto);
+};
